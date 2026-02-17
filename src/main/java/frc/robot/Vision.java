bfebd3ade05e6090f2711b827dfda04ec8b50e85
RushanMehta <meht9068@wths.net>
2026-02-17 17:07:07 -0600

    Update Vision.java

---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class Vision{
+    private PhotonCamera camera = new PhotonCamera("C922_Pro_Stream_Webcam");
+    private PhotonPipelineResult result = new PhotonPipelineResult();
+
+
+}
