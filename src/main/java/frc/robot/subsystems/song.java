8bda98da6d673caa45e24453a1e5bab8cce0a163
Rushan <tridentrobotics4296@gmail.com>
2026-02-26 15:53:16 -0600

    Update song.java

--- src/main/java/frc/robot/subsystems/song.java
+++ src/main/java/frc/robot/subsystems/song.java
@@ -1,21 +1,25 @@
-package frc.robot;
+package frc.robot.subsystems;
 
 // Imports go at the TOP, outside the class
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.CanIDs.*;
 
 public class song extends SubsystemBase{
 
     // Declare your motor and orchestra as class fields
-    private final TalonFX motor1 = new TalonFX(61);
-    private final TalonFX motor2 = new TalonFX(3); // device ID 0, change as needed
+    private final TalonFX motor1 = new TalonFX(shootMotor);
+    private final TalonFX motor2 = new TalonFX(shootMotor2); // device ID 0, change as needed
+    private final TalonFX motor3 = new TalonFX(ArmMotor);
+    private final TalonFX motor4 = new TalonFX(IntakeMotor);
     private final Orchestra m_orchestra = new Orchestra();
 
-    @Override
-    public void robotInit() {
+    public song() {
         m_orchestra.addInstrument(motor1);
         m_orchestra.addInstrument(motor2);
+        m_orchestra.addInstrument(motor3);
+        m_orchestra.addInstrument(motor4);
 
         var status = m_orchestra.loadMusic("output.chrp");
 
@@ -24,14 +28,20 @@ public class song extends SubsystemBase{
         }
     }
 
-    @Override
-    public void teleopInit() {
-        m_orchestra.play(); // Start playing when teleop begins!
-    }
-
-    @Override
-    public void disabledInit() {
-        m_orchestra.stop(); // Stop when disabled
-    }
+        public void playsong() {
+            var status = m_orchestra.play();
+    
+            if (!status.isOK()) {
+                System.out.println("Failed to play music: " + status.toString());
+            }
+        }
+    
+        public void stopsong() {
+            var status = m_orchestra.stop();
+    
+            if (!status.isOK()) {
+                System.out.println("Failed to stop music: " + status.toString());
+            }
+        }
 }
 
