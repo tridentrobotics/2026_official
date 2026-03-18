3c99c193d83a4c031bdaa97f4a71f9f3f074060c
Rushan <tridentrobotics4296@gmail.com>
2026-03-18 18:25:15 -0500

    measurements

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -249,7 +249,7 @@ public class RobotContainer {
         shoot.setDefaultCommand(
                 Commands.run(() -> {
 
-                    double speed = 1;
+                    double speed = joystick.getRawAxis(5);
 
                     if (joystick.getRawButton(18)) {
                         if (speed > .05 || speed < .05) {
